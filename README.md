545cda22203debce60c16f4e4dc20fd3f5029c34
AdamManselll <adammansellhmb@gmail.com>
2020-08-28 15:14:57 -0700

    Edit README.md. Add first set of Specs to README.md.

---
+++ README.md
@@ -0,0 +1,55 @@
+# Adam Mansell's Pizza Parlor Project
+
+### HTML, CSS, & JS Friday Project for Epicodus, 8/28/2020
+
+## **By Adam Mansell**
+
+## Description
+This project contains an index.html file, a styles.css file, and a scripts.js file. By Using these files I was able to create my Pizza Parlor webpage which offers the users some choices of pizza size, and various toppings. After the user selects their prefered pizza, the program will print the total cost to the screen.
+
+## Setup
+Clone this repository.
+
+Open the project files in your text editor.
+
+To view a file in the browser:
+
+Right click one file at a time & copy it's path.
+
+Open your prefered internet browser and paste the file path to view the file.
+(If using mac, after pasting link in search bar, check for "file" dropdown option before hitting return).
+
+## Specs
+Spec: Selecting a small pizza with no toppings should return twelve dollars.
+- Input: "size = "smallVal""
+- Output: 12<br>
+________________________________
+
+Spec: Selecting a medium pizza with no toppings should return fifteen dollars.
+- Input: "size = "medVal""
+- Output: 15<br>
+________________________________
+
+Spec: Selecting a large pizza with no toppings should return twenty dollars.
+- Input: "size = "lrgVal""
+- Output: 20<br>
+________________________________
+
+Spec:
+- Input:
+- Output: <br>
+________________________________
+
+Spec: 
+- Input: 
+- Output: <br>
+________________________________
+
+## Technologies Used
+For this exercise I used html, css, and js.
+
+## Legal
+_Copyright (c) 2020 Adam Mansell_
+
+
+_This software is licensed under the MIT license._
--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,26 @@ function Pizza(size, toppings) {
   this.size = size,
     this.toppings = toppings
 }
-
 Pizza.prototype.pizzaCost = function () {
   let cost = 0;
   let size = (this.size);
   let toppings = this.toppings;
-  // console.log(size, toppings);
+  console.log(size, toppings);
   let small = 12;
   let medium = 15;
   let large = 20;
   let peporoni = 2;
   let sausage = 3;
   let anchovies = 5;
+
   size == "smallVal" ? cost += small : cost;
   size == "medVal" ? cost += medium : cost;
-  size == "legVal" ? cost += large : cost;
-  console.log(size, toppings);
+  size == "lrgVal" ? cost += large : cost;
   toppings.includes("pep") == true ? cost += peporoni : cost;
-  toppings.includes("pep") == true ? console.log("true") : console.log("false");
   toppings.includes("saus") == true ? cost += sausage : cost;
   toppings.includes("anch") == true ? cost += anchovies : cost;
-
-
-  console.log(cost);
   return cost;
 }
-
-// function topCollect(toppingsInput){
-//   let topArray = [];
-//   for (var i = 0; i < toppingsInput.length; i++) {
-//     let top = toppingsInput.length[i];
-//     console.log(top);
-//     top.checked ? topArray.push(top) : top;
-//   }
-//   console.log(topArray);
-//   return topArray;
-// }
-
-
 $(document).ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
@@ -51,11 +33,8 @@ $(document).ready(function () {
     for (i = 0; i < toppingsInput.length; i++) {
       toppings.push(toppingsInput[i]);
     }
-
-    console.log(toppings)
     let myPizza = new Pizza(size, toppings);
     let myCost = myPizza.pizzaCost();
-    console.log(myCost);
     $("#cost").text(myCost);
     $("#cost-div").show();
   })
