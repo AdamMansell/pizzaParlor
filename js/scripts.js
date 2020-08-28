1573bf93415d410e44bd8ddbe3d3505dc62f7683
AdamManselll <adammansellhmb@gmail.com>
2020-08-28 15:24:58 -0700

    Refactored to move business logic for loop, out of UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-function Pizza(size, toppings) {
+function Pizza(size, toppingsInput) {
   this.size = size,
-    this.toppings = toppings
+    this.toppingsInput = toppingsInput
 }
 Pizza.prototype.pizzaCost = function () {
+  let toppings = [];
+  let toppingsInput = (this.toppingsInput);
   let cost = 0;
   let size = (this.size);
-  let toppings = this.toppings;
-  console.log(size, toppings);
   let small = 12;
   let medium = 15;
   let large = 20;
@@ -14,6 +14,10 @@ Pizza.prototype.pizzaCost = function () {
   let sausage = 3;
   let anchovies = 5;
 
+  for (i = 0; i < toppingsInput.length; i++) {
+    toppings.push(toppingsInput[i]);
+  }
+  
   size == "smallVal" ? cost += small : cost;
   size == "medVal" ? cost += medium : cost;
   size == "lrgVal" ? cost += large : cost;
@@ -26,14 +30,10 @@ $(document).ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
     let size = $("#size").val();
-    let toppings = [];
     let toppingsInput = $("input:checkbox[name=tops]:checked").map(function () {
       return $(this).val();
     })
-    for (i = 0; i < toppingsInput.length; i++) {
-      toppings.push(toppingsInput[i]);
-    }
-    let myPizza = new Pizza(size, toppings);
+    let myPizza = new Pizza(size, toppingsInput);
     let myCost = myPizza.pizzaCost();
     $("#cost").text(myCost);
     $("#cost-div").show();
