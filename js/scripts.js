76ff175ff48df77adcc12019481b6ec8d7a65f46
AdamManselll <adammansellhmb@gmail.com>
2020-08-28 11:10:21 -0700

    Calculate cost of pizza and print it to screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,45 @@
 function Pizza(size, toppings) {
   this.size = size,
-  this.toppings = toppings
+    this.toppings = toppings
 }
 
 Pizza.prototype.pizzaCost = function () {
-  let cost;
+  let cost = 0;
   let size = (this.size);
   let toppings = (this.toppings);
+  console.log(size, toppings);
   let small = 12;
   let medium = 15;
   let large = 20;
-  let pep = 2;
-  let saus = 3;
-  let anch = 5;
-  size = smallVal ? cost += small : cost;
-  size = medVal ? cost += medium : cost;
-  size = legVal ? cost += large : cost;
-  toppings = pep ? cost += peporoni : cost;
-  toppings = saus ? cost += sausage : cost;
-  toppings = anch ? cost += anchovies : cost;
+  let peporoni = 2;
+  let sausage = 3;
+  let anchovies = 5;
+  size == "smallVal" ? cost += small : cost;
+  size == "medVal" ? cost += medium : cost;
+  size == "legVal" ? cost += large : cost;
+  toppings == "pep" ? cost += peporoni : cost;
+  toppings == "saus" ? cost += sausage : cost;
+  toppings == "anch" ? cost += anchovies : cost;
+  console.log(cost);
+  return cost;
 }
 
 
-(document).ready(function () {
+$(document).ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
 
-    const size = $("#pizza-size").val();
-    const toppings = $("#pizza-toppings").val();
-    
+    const size = $("#size").val();
+    const toppings = $("#toppings").val();
+
 
 
     let myPizza = new Pizza(size, toppings);
+    console.log(size, toppings);
     let myCost = myPizza.pizzaCost();
+    console.log(myCost);
+    $("#cost").text(myCost);
+    $("#cost-div").show();
 
 
   })
