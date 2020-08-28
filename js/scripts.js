a24d488e5a3e2827ee217a0d6cf271a91fe409dd
AdamManselll <adammansellhmb@gmail.com>
2020-08-28 12:50:16 -0700

    Add ability to function with radio input. Need to add option to fill out multiple toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -24,29 +24,24 @@ Pizza.prototype.pizzaCost = function () {
   return cost;
 }
 
-function TopCollect(){
-  let topArray = [];
-  for (var i = 0; i < toppings.length; i++) {
-    var top = toppings.length[i];
-    if (top.is(':checked') === false) {
-      continue;
-    } else if (top.is(':checked')) {
-      topArray.push(top);
-    }
-  }
-  console.log(topArray);
-  return topArray;
-}
+// function topCollect(toppingsInput){
+//   let topArray = [];
+//   for (var i = 0; i < toppingsInput.length; i++) {
+//     let top = toppingsInput.length[i];
+//     console.log(top);
+//     top.checked ? topArray.push(top) : top;
+//   }
+//   console.log(topArray);
+//   return topArray;
+// }
 
   $(document).ready(function () {
     $("#form").submit(function (event) {
       event.preventDefault();
       let size = $("#size").val();
-      let toppingsInput = [($("#peporoni").val()), ($("#sausage").val()), ($("#anchovies").val())];
-      let toppings = toppingsInput.TopCollect();
-
-      let myPizza = new Pizza(size, [toppings]);
-      console.log(size, toppings);
+      let toppings = $("input:radio[name=tops]:checked").val();
+      console.log(toppings)
+      let myPizza = new Pizza(size, toppings);
       let myCost = myPizza.pizzaCost();
       console.log(myCost);
       $("#cost").text(myCost);
