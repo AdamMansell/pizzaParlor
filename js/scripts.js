e1a08eadbe359d230f82514c98ffa5f839a10e46
AdamManselll <adammansellhmb@gmail.com>
2020-08-28 14:35:07 -0700

    Fix bug that prevented ternaries to work. All objectives met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 function Pizza(size, toppings) {
   this.size = size,
-    this.toppings = [toppings]
+    this.toppings = toppings
 }
 
 Pizza.prototype.pizzaCost = function () {
   let cost = 0;
   let size = (this.size);
   let toppings = this.toppings;
-  console.log(size, toppings);
+  // console.log(size, toppings);
   let small = 12;
   let medium = 15;
   let large = 20;
@@ -17,10 +17,13 @@ Pizza.prototype.pizzaCost = function () {
   size == "smallVal" ? cost += small : cost;
   size == "medVal" ? cost += medium : cost;
   size == "legVal" ? cost += large : cost;
-  // toppings.includes("pep") ? cost += peporoni : cost;
-  toppings.includes("pep", 0) ? console.log("true") : console.log("false");
-  toppings.includes("saus") ? cost += sausage : cost;
-  toppings.includes("anch") ? cost += anchovies : cost;
+  console.log(size, toppings);
+  toppings.includes("pep") == true ? cost += peporoni : cost;
+  toppings.includes("pep") == true ? console.log("true") : console.log("false");
+  toppings.includes("saus") == true ? cost += sausage : cost;
+  toppings.includes("anch") == true ? cost += anchovies : cost;
+
+
   console.log(cost);
   return cost;
 }
@@ -41,9 +44,14 @@ $(document).ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
     let size = $("#size").val();
-    let toppings = $("input:checkbox[name=tops]:checked").map(function () {
+    let toppings = [];
+    let toppingsInput = $("input:checkbox[name=tops]:checked").map(function () {
       return $(this).val();
-    }).get();
+    })
+    for (i = 0; i < toppingsInput.length; i++) {
+      toppings.push(toppingsInput[i]);
+    }
+
     console.log(toppings)
     let myPizza = new Pizza(size, toppings);
     let myCost = myPizza.pizzaCost();
